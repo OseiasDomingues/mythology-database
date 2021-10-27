da04aef2a912695617feee2220eb3934f937f9b4
OseiasDomingues <oseiasdomingues9@gmail.com>
2021-10-27 19:21:05 -0300

    project spring create and config db-test

---
+++ src/main/java/com/projectsdl/mythologydatabase/MythologyDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.projectsdl.mythologydatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythologyDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythologyDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectsdl/mythologydatabase/MythologyDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectsdl.mythologydatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MythologyDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
