6969ace0adfb1bb64071eba97239eb2e5317ecf2
OseiasDomingues <oseiasdomingues9@gmail.com>
2021-10-27 21:22:26 -0300

    services implementations

--- src/main/java/com/projectsdl/mythologydatabase/services/GodService.java
+++ src/main/java/com/projectsdl/mythologydatabase/services/GodService.java
@@ -2,9 +2,11 @@ package com.projectsdl.mythologydatabase.services;
 
 import com.projectsdl.mythologydatabase.models.GodModel;
 
+import java.util.List;
+
 public interface GodService {
 
-    GodModel findAll();
-    GodModel findById();
-    GodModel save(Long id);
+    List<GodModel> findAll();
+    GodModel findById(Long id);
+    GodModel save(GodModel godModel);
 }
--- src/main/java/com/projectsdl/mythologydatabase/services/impl/GodServiceImpl.java
+++ src/main/java/com/projectsdl/mythologydatabase/services/impl/GodServiceImpl.java
@@ -1,23 +1,31 @@
 package com.projectsdl.mythologydatabase.services.impl;
 
 import com.projectsdl.mythologydatabase.models.GodModel;
+import com.projectsdl.mythologydatabase.repositories.GodRepository;
 import com.projectsdl.mythologydatabase.services.GodService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class GodServiceImpl implements GodService {
+
+    @Autowired
+    GodRepository godRepository;
+
     @Override
-    public GodModel findAll() {
-        return null;
+    public List<GodModel> findAll() {
+        return godRepository.findAll();
     }
 
     @Override
-    public GodModel findById() {
-        return null;
+    public GodModel findById(Long id) {
+        return godRepository.findById(id).orElseThrow(NullPointerException::new);
     }
 
     @Override
-    public GodModel save(Long id) {
-        return null;
+    public GodModel save(GodModel godModel) {
+        return godRepository.save(godModel);
     }
 }
